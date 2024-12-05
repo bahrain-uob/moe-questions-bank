41bd67ddf2555b320a1e61af648feaedbc03c39f
Almutawaj <foetheuniversity2022@gmail.com>
2024-12-05 12:26:02 +0300

    Fix

--- packages/functions/src/getExamsCount.ts
+++ packages/functions/src/getExamsCount.ts
@@ -12,34 +12,30 @@ export async function getExamsCount(event: APIGatewayProxyEvent) {
     "Content-Type": "application/json",
   };
 
+  const params = {
+    TableName: tableName, // Table name
+    IndexName: "examStateIndex", // Specify the index name
+    KeyConditionExpression: "examState = :state", // Query condition
+    ExpressionAttributeValues: {
+      ":state": "pending", // The value you want to match
+    },
+  };  
+
+let result;
   try {
-    console.log(event.queryStringParameters);
-    const queryParams = event.queryStringParameters || {};
-    const examState = queryParams.state;
-    console.log("Exam State:", examState);
-
-    const params = {
-      TableName: tableName, // Table name
-      IndexName: "examStateIndex", // Specify the index name
-      KeyConditionExpression: "examState = :state", // Query condition
-      ExpressionAttributeValues: {
-        ":state": "pending", // The value you want to match
-      },
-    };  
-
     // Query the DynamoDB table and get the count
-    const result = await dynamodb.query(params).promise();
+     result = await dynamodb.query(params).promise();
 
     console.log("Count of exams:", result);
-
-    // Return the count as the response body
-    body = JSON.stringify({ count: result.Count });
 
   } catch (error) {
     console.error("Error querying exams:", error);
     statusCode = 400;
     body = JSON.stringify({ error: "Unable to count exams" });
-  }
+  } finally {
+      // Return the count as the response body
+      body = JSON.stringify({ count: result.Count });
+}
 
   // Return response
   return {

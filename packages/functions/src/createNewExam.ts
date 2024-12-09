384a5463a9541eebd19b97f2eef30ca7b9bf1309
Exortix <fares.ashkar77@gmail.com>
2024-12-09 23:02:53 +0300

    Add dynamic exam generation based on subject presets

--- packages/functions/src/createNewExam.ts
+++ packages/functions/src/createNewExam.ts
@@ -19,10 +19,75 @@ const client = new DynamoDBClient({
 });
 
 const dynamo = DynamoDBDocumentClient.from(client);
-
 const bedrockClient = new BedrockRuntimeClient({ region: "us-east-1" });
+const bedrockAgentClient = new BedrockAgentRuntimeClient({ region: "us-east-1"  });
 
-const modelId = "anthropic.claude-3-5-sonnet-20240620-v1:0";
+enum Language {
+  en = "en",
+  ar = "ar",
+}
+
+interface PromptPreset {
+  expressPrompt: string;
+  retrievalQuery: string | undefined;
+  modelId: string;
+  knowledgeBaseId: string;
+  language: Language;
+}
+
+// dictionary or interface mapping the subject to the model id, knowledgeBaseId, prompt, etc.
+// so that we can dynamically select the model id, knowledgeBaseId, prompt, etc. based on the subject
+const subjectToPromptPreset: Record<string, PromptPreset> = {
+  "ENG102": {
+    modelId: "anthropic.claude-3-5-sonnet-20240620-v1:0",
+    knowledgeBaseId: "EU3Z7J6SG6",
+    retrievalQuery: "ENG102 questions",
+    language: Language.en,
+    expressPrompt:  `
+    Act as a school exam generator and create an exam for ENG102 students. The total duration of the exam should not exceed 2 hours.
+    Make sure the exam examines the students in different aspects sufficiently.
+    
+    For the reading part generate a short 100 words article and create questions on it.
+
+    For the listening part create a short listening script and keep it in the appendix of the exam and make the questions on it.
+
+    The exam should be structured approriately.
+
+    This is the exam structure to follow:
+  Listening Section (Total: 10 marks)
+    Question 1: Create true/false questions on the listening script worth 5 marks.
+    Question 2: Create a match the Statements question on the listening script worth 5 marks.
+
+  Reading Section (Total: 20 marks)
+    Part 1 (Reasoning):
+      Include two sub-questions:
+        a. Match the paragraphs with headings (5 marks).
+        b. Short questions and answers (5 marks).
+    Part 2 (Vocabulary):
+      Generate another article (50 Word).
+      Include two sub-questions:
+        a. True or False (5 marks) on the article.
+        b. Match words from the article with their definitions (5 marks).
+
+    Writing Section (Total: 20 marks)
+      Question 1: A writing task worth 10 marks.
+      Question 2: Another writing task worth 10 marks.
+
+
+      Make sure that all the questions has their marks assigned to them.
+    
+    Take to consideration this relevant information from past exams: $relevant_info$
+    
+    Return only the exam and nothing else.`,
+  },
+  "ARAB101": {
+    modelId: "anthropic.claude-3-5-sonnet-20240620-v1:0", // TODO: change this to the correct model id
+    knowledgeBaseId: "EU3Z7J6SG6", // TODO: change this to the correct knowledgeBaseId
+    retrievalQuery: "ARAB101 questions",
+    language: Language.ar,
+    expressPrompt: ``,
+  },
+};
 
 export async function createExam(event: APIGatewayProxyEvent) {
   if (!client || !dynamo) { 
@@ -48,60 +113,28 @@ export async function createExam(event: APIGatewayProxyEvent) {
   };
 
   let prompt = "";
+
+  // get the subject preset based on the subject
+  const promptPreset = subjectToPromptPreset[data.subject];
+  if (!promptPreset) {
+    throw new Error(`Subject ${data.subject} not found in the subjectToModelIdMap`);
+  }
   
-  const bedrockAgentClient = new BedrockAgentRuntimeClient({ region: "us-east-1" });
   let retrieveCommand = new RetrieveCommand({
-    knowledgeBaseId: "EU3Z7J6SG6",
+    knowledgeBaseId: promptPreset.knowledgeBaseId,
     retrievalConfiguration: {
       vectorSearchConfiguration: {
         numberOfResults: 10,
       },
     },
     retrievalQuery: {
-      text: "ENG102 questions",
+      text: promptPreset.retrievalQuery,
     },
   });
 
   if (!data.customize) {
     const relevant_info = (await bedrockAgentClient.send(retrieveCommand)).retrievalResults?.map(e => e.content?.text).join("\n").toString();
-    prompt = `
-        Act as a school exam generator and create an exam for ENG102 students. The total duration of the exam should not exceed 2 hours.
-        Make sure the exam examines the students in different aspects sufficiently.
-        
-        For the reading part generate a short 100 words article and create questions on it.
-
-        For the listening part create a short listening script and keep it in the appendix of the exam and make the questions on it.
-
-        The exam should be structured approriately.
-
-        This is the exam structure to follow:
-      Listening Section (Total: 10 marks)
-        Question 1: Create true/false questions on the listening script worth 5 marks.
-        Question 2: Create a match the Statements question on the listening script worth 5 marks.
-
-      Reading Section (Total: 20 marks)
-        Part 1 (Reasoning):
-          Include two sub-questions:
-            a. Match the paragraphs with headings (5 marks).
-            b. Short questions and answers (5 marks).
-        Part 2 (Vocabulary):
-          Generate another article (50 Word).
-          Include two sub-questions:
-            a. True or False (5 marks) on the article.
-            b. Match words from the article with their definitions (5 marks).
-
-        Writing Section (Total: 20 marks)
-          Question 1: A writing task worth 10 marks.
-          Question 2: Another writing task worth 10 marks.
-
-
-          Make sure that all the questions has their marks assigned to them.
-        
-        Take to consideration this relevant information from past exams: ${relevant_info}
-        
-        Return only the exam and nothing else.
-        
-        `;
+    prompt = subjectToPromptPreset[data.subject].expressPrompt.replace('$relevant_info$', relevant_info? relevant_info : "");
   } else {
     prompt = `
         Act as a school exam generator and create an exam for grade ${data.class} ${data.subject} students.
@@ -135,7 +168,7 @@ export async function createExam(event: APIGatewayProxyEvent) {
     ];
 
     const command = new ConverseCommand({
-      modelId,
+      modelId: promptPreset.modelId,
       //@ts-ignore
       messages: conversation,
       inferenceConfig: { maxTokens: 1200, temperature: 0.5, topP: 0.9 },
